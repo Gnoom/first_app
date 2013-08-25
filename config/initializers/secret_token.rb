e62996b9ff9c64d9999f6eef999465bb7e60dbda
Gnoom <tgnoom@gmail.com>
2013-08-25 19:19:39 +0900

    Initialize repository

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+FirstApp::Application.config.secret_key_base = 'd95be3670fe2b2bfa8630c465caa1f409d6e7c2588b21052ceb7edafbca6a10cc9346554dff9380f13c1dadc13ec2b37e77376078f143fc95273fea6d787803e'
